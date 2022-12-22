5052eb5768d0836b02d3eec7e73626063c6f7a6a
denise877 <deniselll877@gmail.com>
2022-12-21 23:33:24 -0500

    commit

--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,7 @@
-utils::globalVariables(c("X", "Y", "Z", "value"))
+utils::globalVariables(c("X", "Y", "Z", "value", "LBTEST", "SUBJID", "VISITDY",
+                         "LBBASE", "LBSTRESN", "lab_standard", "max_labs",
+                         "BMMTR15", "BMMTNM16", "change_of_albumin",
+                         "BMMTNM4", "BMMTR6", "BMMTR16", "biomarker", "RACE",
+                         "AGE", "B_HEIGHT", "BMMTNM1", "BMMTNM2", "BMMTNM3",
+                         "BMMTNM15", "BMMTR1", "BMMTR2", "BMMTR3",
+                         "SEX", "B_WEIGHT", "PRSURG", "DTH", "TRT"))
